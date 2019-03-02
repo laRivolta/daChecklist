65da202a2f7cbd9e3aa9546448e53a860fdc77b3
Fran Mosteiro <fmosteiro@theinit.com>
2019-03-02 19:58:52 +0100

    We can now: add, mark as completed and delete in pack our tasks. Also added local storage

--- src/app/pages/todo/todo.module.ts
+++ src/app/pages/todo/todo.module.ts
@@ -4,7 +4,7 @@ import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
-import { HomePage } from './home.page';
+import { TodoPage } from './todo.page';
 
 @NgModule({
   imports: [
@@ -14,10 +14,10 @@ import { HomePage } from './home.page';
     RouterModule.forChild([
       {
         path: '',
-        component: HomePage
+        component: TodoPage
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [TodoPage]
 })
-export class HomePageModule {}
+export class TodoPageModule {}
